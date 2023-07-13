f4f1aa2473e4f76a76ea38a1a1c483f48c2a93a2
BaekSeungW00 <ph1losophe@naver.com>
2023-07-14 08:24:57 +0900

    발표용

--- employ/models.py
+++ employ/models.py
@@ -8,19 +8,19 @@ class Postable(models.Model):
     userable = models.ForeignKey('account.Userable', on_delete=models.CASCADE, null=True)
 
 class Employ_post(Postable):
-    required_num = models.IntegerField()
-    start_date = models.DateTimeField(max_length=20)
-    end_date = models.DateTimeField(max_length=20)
-    prefer_condition = models.CharField(max_length=30)
+    required_num = models.IntegerField(null=True)
+    start_date = models.DateTimeField(max_length=20, null=True)
+    end_date = models.DateTimeField(max_length=20, null=True)
+    prefer_condition = models.CharField(max_length=30, null=True)
     image = models.ImageField(upload_to='post/employ/', null=True)
     CAREER_CHOICES = (('경력', '경력'), ('경력', '신입'))
-    career = models.CharField(max_length=100, default='a', choices=CAREER_CHOICES)
+    career = models.CharField(max_length=100, default='a', choices=CAREER_CHOICES, null=True)
     EMPLOY_SHAPE_CHOICES = (('인턴','인턴'),('정규직','정규직'),('비정규직','비정규직'))
-    employ_shape = models.CharField(max_length=100, default='a', choices=EMPLOY_SHAPE_CHOICES )
-    apply_method = models.CharField(max_length=50)
+    employ_shape = models.CharField(max_length=100, default='a', choices=EMPLOY_SHAPE_CHOICES , null=True)
+    apply_method = models.CharField(max_length=50, null=True)
 
 class Freepost_e (Postable):
-    image = models.ImageField(upload_to="post/free_post_e")
+    image = models.ImageField(upload_to="post/free_post_e", null=True, blank=True)
 
 
 class Question(Postable):
